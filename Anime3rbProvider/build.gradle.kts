13a764f4bcd69178df85821be3934eaf5476583f
josefliox600 <joseflioxi11@gmail.com>
2026-03-16 07:17:48 +0000

    Create build.gradle.kts

---
+++ Anime3rbProvider/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 1
+
+android {
+    namespace = "com.anime3rb"
+}
+
+cloudstream {
+    language = "ar"
+    authors = listOf("Josef")
+    status = 1
+
+    tvTypes = listOf(
+        "Anime"
+    )
+
+    iconUrl = ""
+}
