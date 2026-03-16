ef6df0c3f4c318d15009b9d947577ceb93207965
josefliox600 <joseflioxi11@gmail.com>
2026-03-16 04:07:39 +0000

    Create build.gradle.kts

---
+++ EgibestProvider/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    language = "ar"
+    authors = listOf("Josef")
+    status = 1
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+
+    iconUrl = ""
+}
